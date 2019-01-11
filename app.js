d81b22b949bab9387a762587b2acf40a1c19110a
Jonathan Peters <42306733+JonathanPetersCode@users.noreply.github.com>
2019-01-11 00:00:49 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+const mic = document.querySelector('#mic');
+const content = document.querySelector('#content');
+const status = document.querySelector(".device-status");
+const statusText = document.querySelector(".device-status").innerHTML;
+let messenger = document.querySelector(".message");
+
+//Mic was clicked and device stopped listening
+mic.onclick = () => {
+    recognition.start();
+}
+//Device stopped listening
+recognition.onend = function () {
+    recognition.stop();
+    status.innerHTML = "Your device stopped listening!"
+}
+
+//Result of speech recognition 
+recognition.onresult = (event) => {
+    const color = event.results[0][0].transcript;
+    content.style.backgroundColor = color;
+    messenger.innerHTML = `I heard you say : "${event.results[0][0].transcript.toUpperCase()}!"  `;
+}
+
+//Device cannot make a match - "onnomatch" Method does not work on Firefox
+recognition.onnomatch = function () {
+       console.log('Speech not recognised');
+   }
